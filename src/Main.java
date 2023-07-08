56c77c300f65d47b537fb61c43db2bd1c8734d7a
Денис <tequila96@bk.ru>
2023-07-08 16:22:49 +0500

    Повторение,мать учения

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, человечище!");
+        System.out.println("Привет, мой дорогой друг");
     }
 }
